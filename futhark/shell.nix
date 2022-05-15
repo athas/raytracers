47f651034d10e2fa84ba24f9213a76bfae17771b
Troels Henriksen <athas@sigkill.dk>
2022-05-15 20:13:01 +0200

    Use multicore backend by default.

--- futhark/shell.nix
+++ futhark/shell.nix
@@ -2,7 +2,8 @@ with import (builtins.fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/820177eded91f3908cfc72dfee00e831ea3d0060.zip";
   sha256 = "1yqx5zy154f8057inwjp2ranizgilvpblqq31cy7nryrwj2apics";
 }) {};
-stdenv.mkDerivation {
-    name = "ray";
-    buildInputs = [ futhark ocl-icd opencl-headers ];
+stdenvNoCC.mkDerivation {
+  name = "ray";
+  CC = "clang";
+  buildInputs = [ clang futhark ];
 }
